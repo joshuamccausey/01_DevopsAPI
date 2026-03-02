eea4ad3414edc26ec1011619d99a9c30c593ab0d
Joshua <joshuamccausey@gmail.com>
2026-03-02 06:58:02 -0500

    Editing the main python code under the FastAPI server to send out API calls and get the weather based on the zip of the requester

--- application_code/main.py
+++ application_code/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+import requests
 
 app = FastAPI()
 
@@ -9,11 +10,23 @@ class Item(BaseModel):
     is_offer: bool | None = None
 
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+@app.get("/current_weather")
+def get_weather():
+    location = input("Enter your zip code")
+    response = requests.get("http://api.openweathermap.org/geo/1.0/zip?zip={zip code},{country code}&appid={API key}")
+    data = response.json()
+    lat = data["lat"]
+    lon = data["lon"]
+    api_key = "89be9949d4f3e957623120d37377899f"
+    weather_response = requests.get(f"http://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={api_key}")
+    weather_data = weather_response.json()
+    return weather_data
 
 
+
+
+
+"""
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: str | None = None):
     return {"item_id": item_id, "q": q}
@@ -21,3 +34,5 @@ def read_item(item_id: int, q: str | None = None):
 @app.put("/items/{item_id}")
 def update_item(item_id: int, item: Item):
     return {"item_name": item.name, "item_id": item_id}
+"""
+
