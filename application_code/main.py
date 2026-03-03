2e85680cb86b017adbce5111155f1137ae293280
Joshua <joshuamccausey@gmail.com>
2026-03-03 06:17:10 -0500

    Updated get_weather as it was setup using input function to collect zip. Now client has to pass data using GET request

--- application_code/main.py
+++ application_code/main.py
@@ -5,8 +5,7 @@ import requests
 app = FastAPI()
 
 
-def get_weather():
-    location = input("Enter your zip code: ")
+def get_weather(location: str):
     api_key = "89be9949d4f3e957623120d37377899f"
     response = requests.get(f"http://api.openweathermap.org/geo/1.0/zip?zip={location},US&appid={api_key}")
     data = response.json()
@@ -17,13 +16,11 @@ def get_weather():
     return weather_data
 
 
-@app.get("/current_weather")
-def current_weather():
-    weather_data = get_weather()
-    print("Expect " + str(weather_data["weather"][0]["description"]) + " with an average temperature of " + str(weather_data["main"]["temp"]) + " degrees Fahrenheit.")
+@app.get("/current_weather/{location}")
+def current_weather(location: str):
+    weather_data = get_weather(location)
     return "Expect " + str(weather_data["weather"][0]["description"]) + " with an average temperature of " + str(weather_data["main"]["temp"]) + " degrees Fahrenheit."
 
-current_weather()
 
 
 
