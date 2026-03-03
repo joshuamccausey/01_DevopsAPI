dd3ce90bdd88ecd7d4c27215835a12343bbe1644
Joshua <joshuamccausey@gmail.com>
2026-03-03 06:04:17 -0500

    API calls to openweathermap.org are now working as expected and the program is returning the condiditions based on users input (zip)

--- application_code/main.py
+++ application_code/main.py
@@ -6,13 +6,13 @@ app = FastAPI()
 
 
 def get_weather():
-    location = input("Enter your zip code")
+    location = input("Enter your zip code: ")
     api_key = "89be9949d4f3e957623120d37377899f"
     response = requests.get(f"http://api.openweathermap.org/geo/1.0/zip?zip={location},US&appid={api_key}")
     data = response.json()
     lat = data["lat"]
     lon = data["lon"]
-    weather_response = requests.get(f"http://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={api_key}")
+    weather_response = requests.get(f"http://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={api_key}&units=imperial")
     weather_data = weather_response.json()
     return weather_data
 
@@ -20,10 +20,10 @@ def get_weather():
 @app.get("/current_weather")
 def current_weather():
     weather_data = get_weather()
-    return weather_data
+    print("Expect " + str(weather_data["weather"][0]["description"]) + " with an average temperature of " + str(weather_data["main"]["temp"]) + " degrees Fahrenheit.")
+    return "Expect " + str(weather_data["weather"][0]["description"]) + " with an average temperature of " + str(weather_data["main"]["temp"]) + " degrees Fahrenheit."
 
-
-get_weather()
+current_weather()
 
 
 
