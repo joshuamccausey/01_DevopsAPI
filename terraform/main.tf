293d0f57f42063225f0c6019fccd8df22683cbf4
Joshua <joshuamccausey@gmail.com>
2026-03-10 06:34:18 -0400

    Modified terraform code for project environment

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,7 +6,7 @@ data "aws_ami" "ubuntu" {
   most_recent = true
 
   filter {
-    name = "name"
+    name   = "name"
     values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-*"]
   }
 
