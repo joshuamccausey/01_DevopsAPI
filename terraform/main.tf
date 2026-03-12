83637efceab49a8dbaa18a94fc11a7d454abdfd8
Joshua <joshuamccausey@gmail.com>
2026-03-12 06:21:23 -0400

    Added further networking infra using Terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -13,14 +13,19 @@ data "aws_ami" "ubuntu" {
   owners = ["099720109477"] # Canonical
 }
 
+
+# Create an EC2 key pair for SSH access
 resource "aws_key_pair" "deployer" {
   key_name = "developer_key"
   public_key = file("~/.ssh/aws_ec2_key.pub")
 }
 
+# Networking resources
+
 resource "aws_security_group" "app_server_sg" {
   name        = "app_server_sg"
   description = "Allow HTTP traffic to app server"
+  vpc_id = aws_vpc.main.id
 
   ingress {
     from_port   = 22
@@ -36,14 +41,54 @@ resource "aws_security_group" "app_server_sg" {
     cidr_blocks = ["66.187.96.118/32"]
   }
 
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"] 
+
 }
+}
+
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_subnet" "main" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "us-east-2a"
+}
+
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+}
+
+resource "aws_route_table" "main" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
+  }
+}
+
+resource "aws_route_table_association" "main" {
+  subnet_id      = aws_subnet.main.id
+  route_table_id = aws_route_table.main.id
+}
+
+
+# EC2 instance for the application server
 
 resource "aws_instance" "app_server" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = "t4g.micro"
   associate_public_ip_address = true
   key_name = aws_key_pair.deployer.key_name
-  security_groups = [aws_security_group.app_server_sg.name] 
+
+  subnet_id = aws_subnet.main.id
+  vpc_security_group_ids = [aws_security_group.app_server_sg.id] 
 
   tags = {
     Name = "01_devopsapi_app_server"
