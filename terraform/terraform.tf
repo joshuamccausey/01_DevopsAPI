e9b60da6270d91b9011541249b1135a63d286dad
Joshua <joshuamccausey@gmail.com>
2026-03-22 08:03:16 -0400

    Got s3 statefile working

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -5,6 +5,12 @@ terraform {
       version = "~> 5.92"
     }
   }
+  # Create state storage buck
+backend "s3" {
+    bucket = "01-devopsapi-state"
+    key    = "terraform.tfstate"
+    region = "us-east-2"
+  }
 
   required_version = ">= 1.2"
 }
